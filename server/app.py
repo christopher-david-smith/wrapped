d42851693c86818f991a0172c2d33384860db2b0
Christopher Smith <christopher.smith@qoria.com>
2025-11-15 11:42:48 +0000

    Update server to accept requests from client

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database import create_db_and_tables
 from .routers import gifts
@@ -6,4 +7,16 @@ from .routers import gifts
 
 app = FastAPI()
 app.include_router(gifts.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 create_db_and_tables()
